cc466093cd468add590617454e532c1f201ded28
manzi-chriss <manzichrss6@gmail.com>
2024-08-31 18:36:06 +0200

    removed db url

--- model/DBconnection.js
+++ model/DBconnection.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.DB);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
