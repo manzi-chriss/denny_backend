e2cc1ce8e2fcd8ffda28f04c34bf8290d7f81c12
manzi-chriss <manzichrss6@gmail.com>
2024-09-01 13:45:50 +0200

    changed

--- index.js
+++ index.js
@@ -46,4 +46,7 @@ app.use('/message',messageRoute)
 
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => 
+    // console.log(`Server running on port ${PORT}`
+        
+    ));
